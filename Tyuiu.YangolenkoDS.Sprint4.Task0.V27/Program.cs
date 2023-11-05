af7751900977a68c812cff529ef6706a5b6a4537
Tyuiu.YangolenkoDS@outlook.com <Tyuiu.YangolenkoDS@outlook.com>
2023-11-05 18:39:26 +0500

    Task0

--- Tyuiu.YangolenkoDS.Sprint4.Task0.V27/Program.cs
+++ Tyuiu.YangolenkoDS.Sprint4.Task0.V27/Program.cs
@@ -16,17 +16,17 @@ namespace Tyuiu.YangolenkoDS.Sprint4.Task0.V27
 
             Console.Title = "Спринт #4 | Выполнил: Янголенко Д. С. | АСОиУб-23-1";
             Console.WriteLine("*************************************************************************************");
-            Console.WriteLine("* Спринт #4 *");
-            Console.WriteLine("* Тема: Одномерные массивы (статический ввод) *");
-            Console.WriteLine("* Задание #0 *");
-            Console.WriteLine("* Вариант #27 *");
-            Console.WriteLine("* Выполнил: Янголенко Данил Сергеевич | АСОиУб-23-1 *");
+            Console.WriteLine("* Спринт #4                                                                         *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                                       *");
+            Console.WriteLine("* Задание #0                                                                        *");
+            Console.WriteLine("* Вариант #27                                                                       *");
+            Console.WriteLine("* Выполнил: Янголенко Данил Сергеевич | АСОиУб-23-1                                 *");
             Console.WriteLine("*************************************************************************************");
-            Console.WriteLine("* УСЛОВИЕ: *");
-            Console.WriteLine("* Подсчитать произведение нечетных элементов массива. *");
-            Console.WriteLine("* *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Подсчитать произведение нечетных элементов массива.                               *");
+            Console.WriteLine("*                                                                                   *");
             Console.WriteLine("*************************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
             Console.WriteLine("*************************************************************************************");
 
             int[] array = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
@@ -35,11 +35,11 @@ namespace Tyuiu.YangolenkoDS.Sprint4.Task0.V27
             Console.WriteLine("Массив = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 }");
 
             Console.WriteLine("*************************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ: *");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
             Console.WriteLine("*************************************************************************************");
 
             Console.WriteLine("Произведение нечетных элементов массива = " + res);
-            Console.ReadKey(); 
+            Console.ReadKey();
         }
     }
 }
